693f76cb3eb610fd14dde8c9db3cffdefec6769d
jianfenglihg <jianfenglihg@gmail.com>
2020-03-12 20:38:57 +0800

    update

--- models/__init__.py
+++ models/__init__.py
@@ -1,17 +1,4 @@
 #from .back2future_copy import Model as Back2Future
 from .back2future import Model as Back2Future
 from .back2future_single import Model as Back2FutureS
-from .DispNetS import DispNetS
-from .DispNetS6 import DispNetS6
-from .DispResNet6 import DispResNet6
-from .DispResNetS6 import DispResNetS6
-from .FlowNetC6 import FlowNetC6
-from .MaskNet6 import MaskNet6
-from .MaskResNet6 import MaskResNet6
-from .PoseExpNet import PoseExpNet
-from .PoseNet6 import PoseNet6
-from .PoseNetB6 import PoseNetB6
-from .MoveNet6 import MoveNet6
-from .MoveResNet6 import MoveResNet6
-from .PoseNet import PoseNet
 from .unflow import Network as UnFlow
