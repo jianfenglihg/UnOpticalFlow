e25953b0daa24d71962c685bc47cdb6a68b6c1b6
l50012366 <lijianfeng18@huawei.com>
2020-07-29 09:21:16 +0800

    delete original

--- models/__init__.py
+++
@@ -1,4 +0,0 @@
-#from .back2future_copy import Model as Back2Future
-from .back2future import Model as Back2Future
-from .back2future_single import Model as Back2FutureS
-from .unflow import Network as UnFlow
