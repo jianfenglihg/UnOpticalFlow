e7f9c82df251e485b1de68c1f8c688e1ab8502c1
haiyangpeng <haiyangpengai@gmail.com>
2021-09-30 11:51:01 +0800

    fix the issue of identical get_data_list method in KITTI_2015 and KITTI_2012.

--- core/dataset/kitti_2012.py
+++ core/dataset/kitti_2012.py
@@ -22,9 +22,9 @@ class KITTI_2012(KITTI_Prepared):
         data_list = []
         for i in range(self.num_total):
             data = {}
-            data['img1_dir'] = os.path.join(self.data_dir, 'image_2', str(i).zfill(6) + '_10.png')
-            data['img2_dir'] = os.path.join(self.data_dir, 'image_2', str(i).zfill(6) + '_11.png')
-            data['calib_file_dir'] = os.path.join(self.data_dir, 'calib_cam_to_cam', str(i).zfill(6) + '.txt')
+            data['img1_dir'] = os.path.join(self.data_dir, 'colored_0', str(i).zfill(6) + '_10.png')
+            data['img2_dir'] = os.path.join(self.data_dir, 'colored_0', str(i).zfill(6) + '_11.png')
+            data['calib_file_dir'] = os.path.join(self.data_dir, 'calib', str(i).zfill(6) + '.txt')
             data_list.append(data)
         return data_list
 
--- core/dataset/kitti_2015.py
+++ core/dataset/kitti_2015.py
@@ -9,6 +9,16 @@ class KITTI_2015(KITTI_2012):
 
         self.data_list = self.get_data_list()
 
+    def get_data_list(self):
+        data_list = []
+        for i in range(self.num_total):
+            data = {}
+            data['img1_dir'] = os.path.join(self.data_dir, 'image_2', str(i).zfill(6) + '_10.png')
+            data['img2_dir'] = os.path.join(self.data_dir, 'image_2', str(i).zfill(6) + '_11.png')
+            data['calib_file_dir'] = os.path.join(self.data_dir, 'calib_cam_to_cam', str(i).zfill(6) + '.txt')
+            data_list.append(data)
+        return data_list        
+
 if __name__ == '__main__':
     pass
 
